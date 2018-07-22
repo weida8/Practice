49e00fb311578b25da15ad5e40fd053ebfc0e528
weida8 <wei-dapan@Wei-Das-MacBook-Air.local>
2018-07-22 16:28:51 -0500

    added set data

--- single_linked_list.py
+++ single_linked_list.py
@@ -1,17 +1,22 @@
 class Node(Object):
 
-    def _init_(self, data=None, next_node=None):
-        self.data = data
-        self.next_node = next_node
+    def _init_(self, initdata):
+        self.data = initdata
+        self.next= None
 
     def get_data(self):
         return self.data
 
     def get_next(self):
-        return self.next_node
+        return self.next
+
+    def set_data(self, new_data):
+        self.data = new_data
 
     def set_next(self, new_next):
         self.set_next = new_next
 
     def _str_(self):
         return (str(self.data))
+
+class Linkedlist(self, )
